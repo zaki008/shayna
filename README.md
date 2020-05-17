f248e20a36454efe91b1ad91df3bf7c8dbd100fb
zaki008 <zakigerrard10@gmail.com>
2020-05-17 20:17:50 +0700

    upload project vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # shayna
-Project Aplikasi Vue Sebagai bahan belajar kelas Full-Stack Laravel Vue JS (Toko Online)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="home">
+  <Header />
+  <HeroShayna />
+  <WomanShayna />
+  <InstaShayna />
+  <PantnerShayna />
+  <Footer />
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PantnerShayna from '@/components/PantnerShayna.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    PantnerShayna,
+    Footer
+  }
+}
+</script>
